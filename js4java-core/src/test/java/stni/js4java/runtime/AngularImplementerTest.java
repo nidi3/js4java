ed8076b93f234fdc4a9db68d8da3cee623d6fdb8
Stefan Niederhauser <stefan.niederhauser@mimacom.com>
2013-12-28 05:55:43 +0100

    test to show problem with Boolean -> js conversion

--- js4java-core/src/test/java/stni/js4java/runtime/AngularImplementerTest.java
+++ js4java-core/src/test/java/stni/js4java/runtime/AngularImplementerTest.java
@@ -2,6 +2,7 @@ package stni.js4java.runtime;
 
 import org.junit.Test;
 
+import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.io.InputStreamReader;
@@ -14,8 +15,18 @@ import static org.junit.Assert.assertEquals;
  */
 public class AngularImplementerTest {
     interface Demo {
-        String sub(String s, int from, int to);
+        String sub(String s, Integer from, int to);
+
+        boolean test(Data d);
     }
+
+   public static class Data {
+       public Boolean value;
+
+       public Data(boolean value) {
+           this.value = value;
+       }
+   }
 
     @Test
     public void simple() throws Exception {
@@ -26,13 +37,15 @@ public class AngularImplementerTest {
     private void assertSimple(Reader angular) throws Exception {
         final ScriptEngineManager manager = new ScriptEngineManager();
         final ScriptEngine js = manager.getEngineByExtension("js");
-        final AngularImplementer ai = new AngularImplementer(js,angular);
+        final AngularImplementer ai = new AngularImplementer(js, angular);
         ai.module("util");
-        ai.service("util", "str", "this.sub=function(s,a,b){return s.substring(a,b);}");
+        ai.service("util", "str", "this.sub=function(s,a,b){return s.substring(a,b);}; this.test=function(data){println(data.value);return !data.value;};");
         ai.bootstrap("app", "util");
         ai.bootstrapDefault("util");
+        final ScriptContext context = js.getContext();
         final Demo appDemo = ai.implementBy(Demo.class, "app.str");
         final Demo defaultDemo = ai.implementBy(Demo.class, "str");
+        System.out.println(appDemo.test(new Data(false)));
         assertEquals("23", appDemo.sub("1234", 1, 3));
         assertEquals("23", defaultDemo.sub("1234", 1, 3));
     }
